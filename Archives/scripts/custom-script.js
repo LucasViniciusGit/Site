8b2dfb8365b96556d789f9d11173a628f5b84837
Lucas Vinicius <lucasvp29@hotmail.com>
2014-06-29 12:44:05 -0300

    Definição completa do layout
    Alteração na hom page, defindo nosso novo layout do site

--- Archives/scripts/custom-script.js
+++ Archives/scripts/custom-script.js
@@ -33,7 +33,7 @@ function getdate()
 	if (!document.all&&!document.getElementById)
 		getdate();
 
-function setdate()
+function Setdate()
 {
 	if (document.all||document.getElementById)
 		setInterval("getdate()",10);
@@ -56,4 +56,97 @@ function SetIdade() {
         quantos_anos--;
     }
     document.getElementById("setIdade").innerHTML = quantos_anos +" anos";
+}
+
+/* Função que retorna o Bom dia, Boa tarde ou Boa noite. */
+function SetCumprimento()
+{
+	var data = new Date();
+	var horas = data.getHours();
+	var exibe = "";
+
+	if(horas > 0 && horas <= 11)
+		exibe = "Bom dia";
+	else if (horas > 11 && horas <= 18)
+		exibe = "Boa tarde";
+	else
+		exibe = "Boa noite";
+
+	document.getElementById("cumprimento").innerHTML = exibe;
+
+}
+
+function SetMessage()
+{
+	var rdn = Math.floor((Math.random() * 20) + 1);
+	var message = "";
+
+	switch(rdn)
+	{
+		case 1:
+			message = "“É fácil reprovar uma ideia, difícil é ter uma.” - Homer Simpson ";
+			break;
+		case 2:
+			message = "“O que dói não é crescer. O que dói é ver uma pessoa que tanto amou mudar.” - Naruto";
+			break;
+		case 3:
+			message = "“Por um mundo onde as baterias duram pelo menos 2 dias.” ";
+			break;
+		case 4:
+			message = "“Se tudo um dia falhar... pressione o ctrl+alt+del.”";
+			break;
+		case 5:
+			message = "“Você nunca sabe a força que tem. Até que a sua única alternativa é ser forte.” – Johnny Depp ";
+			break;
+		case 6:
+			message = "“Há mais pessoas que desistem do que pessoas que fracassam.” – Henry Ford";
+			break;
+		case 7:
+			message = "“A alegria evita mil males e prolonga a vida.” – William Shakespeare";
+			break;
+		case 8:
+			message = "“As loiras roubam a cena, já as morenas roubam o filme todo.”";
+			break;
+		case 9:
+			message = "“E pensar que a gente trabalha por um simples pedaço de papel.”";
+			break;
+		case 10:
+			message = "“Por um país mais justo, onde quem trabalha mais, ganha mais.”";
+			break;
+		case 3:
+			message = "“Uma boa equipe é o resultado de uma boa liderança.”";
+			break;
+		case 11:
+			message = "“Regra da vida: Se eu te chamei para conversar ontem, hoje quem tem que chamar é você.”";
+			break;
+		case 12:
+			message = "“Se você não tem razão para estar vivo, é o mesmo que estar morto.” - Naruto";
+			break;
+		case 13:
+			message = "“Quando se tem humildade, a aparência é um pequeno detalhe.”";
+			break;
+		case 14:
+			message = "“Não vale a pena viver sonhando e se esquecer de viver.” - Harry Potter";
+			break;
+		case 15:
+			message = "“Não podemos alterar o futuro.” - Back to the future ";
+			break;
+		case 16:
+			message = "“Gente que tem coragem não finge” - Rodox";
+			break;
+		case 17:
+			message = "“E agora está claro como esta promessa que estamos fazendo” - Justin Timberlake (Mirros) ";
+			break;
+		case 18:
+			message = "“O prazer no trabalho aperfeiçoa a obra” - Aristóteles";
+			break;
+		case 19:
+			message = "“Mais importante que a vontade de vencer é a coragem de começar.”";
+			break;
+		case 20:
+			message = "“Sem lutas não haverá vitória, sem incentivo não haverá vontade.”";
+			break;
+	}
+
+	document.getElementById("message").innerHTML = message;
 }
